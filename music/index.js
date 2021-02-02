7088df524dd0f36b3b8d5a2a156e362ec9764f6c
Jack Devey <jack@bandev.uk>
2021-02-02 17:41:54 +0000

    fix

--- music/index.js
+++ music/index.js
@@ -3,7 +3,7 @@ module.exports = class Music{
     async request(message, params, player){
         // If there's already a song playing
             // Else, play the song
-            let song1 = await player.play(message.member.voice.channel, "rickroll");
+            let song1 = await player.play(message.member.voice.channel, params.join(" "));
             var song2 = song1.song;
             message.channel.send(`Started playing ${song2.name}!`);
 
