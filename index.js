513030b65ddd2f3d87ebfa765c0f3f3d69d9e6b1
Jack Devey <jack@bandev.uk>
2021-02-02 17:06:02 +0000

    added rickroll

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const Discord = require("discord.js"); // imports the discord library
 require('dotenv').config();
+const { Player }  = require("discord-music-player");
 
 const client = new Discord.Client(); // creates a discord client
 const token = process.env.TOKEN // gets your token from the file
+
+const player = new Player(client, {
+    leaveOnEnd: false,
+    leaveOnStop: false,
+    leaveOnEmpty: true,
+    timeout: 0,
+    volume: 150,
+    quality: 'high',
+});
+
+client.player = player;
 
 const Core = require('./core');
 var core = new Core();
@@ -13,7 +25,11 @@ var people = new People();
 const Plug = require('./plug');
 var plug = new Plug();
 
+const Music = require('./music');
+var music = new Music();
+
 const Games = require('./games');
+const MusicPlayerError = require("discord-music-player/src/MusicPlayerError");
 var games = new Games(client);
 
 client.once("ready", () => { // prints "Ready!" to the console once the bot is online
@@ -22,46 +38,49 @@ client.once("ready", () => { // prints "Ready!" to the console once the bot is o
 });
 
 client.on("message", message => { // runs whenever a message is sent
-    switch(message.content){
-        case "?hello":
-            core.hello(message.channel)
-            break; 
-        case "?ballmer":
-            core.ballmer(message.channel)
-            break;
-        case "?developers":
-            core.developers(message.channel)
-            break;
-        case "?russell":
-            people.russell(message.channel)
-            break;
-        case "?hannah":
-            people.hannah(message.channel)
-            break;
-        case "?bandev":
-            plug.bandev(message.channel)
-            break;
-        case "?kickrus":
-            people.kick(message.channel, "russell")
-            core.ballmer(message.channel)
-            break;
-        case "?hollie":
-            people.hollie(message.channel)
-            break;
-        case "?jack":
-            people.jack(message.channel)
-            break;
-        case "?simon":
-            core.simon(message.channel)
-            break;
-        case "?bigd":
-            people.bigd(message.channel)
-            break;
-    }
     if(message.content.startsWith("?del")){
         core.delete(message, message.content.split(" "))
     }else if(message.content.startsWith("?hangman")){
         games.startHangman(message, message.content.split(" "))
+    }else if(message.content.startsWith("?play")){
+        music.request(message, message.content.split(" "), player)
+    }else{
+        switch(message.content){
+            case "?hello":
+                core.hello(message.channel)
+                break; 
+            case "?ballmer":
+                core.ballmer(message.channel)
+                break;
+            case "?developers":
+                core.developers(message.channel)
+                break;
+            case "?russell":
+                people.russell(message.channel)
+                break;
+            case "?hannah":
+                people.hannah(message.channel)
+                break;
+            case "?bandev":
+                plug.bandev(message.channel)
+                break;
+            case "?kickrus":
+                people.kick(message.channel, "russell")
+                core.ballmer(message.channel)
+                break;
+            case "?hollie":
+                people.hollie(message.channel)
+                break;
+            case "?jack":
+                people.jack(message.channel)
+                break;
+            case "?simon":
+                core.simon(message.channel)
+                break;
+            case "?bigd":
+                people.bigd(message.channel)
+                break;
+        }
     }
 });
 
---
+++ music/index.js
@@ -0,0 +1,12 @@
+module.exports = class Music{
+
+    async request(message, params, player){
+        // If there's already a song playing
+            // Else, play the song
+            let song1 = await player.play(message.member.voice.channel, "rickroll");
+            var song2 = song1.song;
+            message.channel.send(`Started playing ${song2.name}!`);
+
+    }
+
+}
