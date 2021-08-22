efb9e5b5ac77e19194e515118c05cbec80121b8c
Jack Devey <jack@bandev.uk>
2021-08-22 23:31:21 +0100

    Convert to Notify

--- index.js
+++ index.js
@@ -1,206 +1,115 @@
-//Discord.js library
+
+
 const Discord = require("discord.js");
 
-//Dotenv library
+const intents = [
+    Discord.Intents.FLAGS.GUILDS,
+    Discord.Intents.FLAGS.GUILD_MEMBERS,
+    Discord.Intents.FLAGS.GUILD_BANS,
+    Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+    Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+    Discord.Intents.FLAGS.GUILD_INVITES,
+    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+    Discord.Intents.FLAGS.GUILD_PRESENCES,
+    Discord.Intents.FLAGS.GUILD_MESSAGES,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+    Discord.Intents.FLAGS.DIRECT_MESSAGES,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+    Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+];
+
+const bot = new Discord.Client({ intents: intents });
+
 require('dotenv').config();
 
-//Discord music player library
-const { Player }  = require("discord-music-player");
+bot.login(process.env.TOKEN)
 
-//Core commands library
-const Core = require('./core');
-var core = new Core();
-
-//Space commands library
-const Space = require('./space');
-var space = new Space();
-
-//Music commands library
-const Music = require('./music');
-var music = new Music();
-
-//Get setting variables
-const token = process.env.TOKEN;
-const server = process.env.SERVER;
-const prefix = process.env.PREFIX;
-const name = process.env.NAME;
-const pfp = process.env.PFP;
-const color = process.env.COLOR;
-const url = process.env.URL;
-const parentId = process.env.PARENTID;
-
-//New discord client instance
-const client = new Discord.Client();
-
-//Define music player settings
-const player = new Player(client, {
-    leaveOnEnd: true,
-    leaveOnStop: true,
-    leaveOnEmpty: true,
-    timeout: 0,
-    volume: 100,
-    quality: 'high',
-});
-
-//Set player as music player
-client.player = player;
-
-//When client logs successfully in
-client.once("ready", () => {
-    //Set the game based on how many servers the bot is in
-    client.user.setPresence({
-        status: 'online',
-        activity: {
-            name: "in " + client.guilds.cache.size + " servers | " + url,
-            type: "PLAYING",
-        }
-    })
-
-    //Discord / commands to help people
-    client.api.applications(client.user.id).commands.post({data: {
-        name: name+'Help',
-        description: 'Get help and commands for ' + name + ' bot.'
-    }})
-
-    //Show debug information in the console for turn on
-    console.log("Build Successful");
-    console.log("=========================");
-    console.log("Configured variables:");
-    console.log("=========================");
-    console.log("server: " + server);
-    console.log("server_count: " + client.guilds.cache.size);
-    console.log("prefix: " + prefix);
-    console.log("name: " + name);
-    console.log("pfp: " + pfp);
-    console.log("color: " + color);
-    console.log("url: " + url);
-    console.log("=========================");
-    console.log("Waiting for commands:");
-    console.log("=========================");
-});
-
-client.player.on('playlistAdd',  (message, queue, playlist) => 
-        message.channel.send(`${playlist.name} playlist with ${playlist.videoCount} songs has been added to the queue!`));
-
-client.on("message", message => {
-    //If message is not meant for us, ignore it
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    //Work out the command that was sent
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
-
-    //Debug things
-    console.log("command recieved: " + command + " " + args);
-
-    switch(command) {
-        case "say":
-            //Play song: play [name/url]
-            message.channel.send(args);
-            break;
-        case "play":
-            //Play song: play [name/url]
-            music.play(message, args, player, Discord, server, prefix);
-            break;
-        case "playlist":
-            //Play playlist: playlist [name/url]
-            music.playlist(message, args, player, Discord, server, prefix);
-            break;
-        case "skip":
-            //Skips the current song: skip
-            music.skip(message, player);
-            break;
-        case "clear":
-            //Clears the queue and leaves: clear
-            music.clear(message, player);
-            break;
-        case "shuffle":
-            //Shuffles the queue: shuffle
-            music.shuffle(message, player);
-            break;
-        case "q":
-            //Shows the queue to the user: q
-            music.queue(message, player);
-            break;
-        case "queue":
-            //Shows the queue to the user: queue
-            music.queue(message, player);
-            break;
-        case "loop":
-            //Loops the current song: loop
-            music.loop(message, player);
-            break;
-        case "pause":
-            //Pauses the current song: pause
-            music.pause(message, player);
-            break;
-        case "resume":
-            //Resumes the current song: resume
-            music.resume(message, player);
-            break;
-        case "progress":
-            //Shows a progress bar for the current song: progress
-            music.progress(message, player);
-            break;
-        case "rover":
-            //Shows a mars rover picture: rover
-            space.rover(message, Discord, args);
-            break;
-        case "help":
-            //Shows a help embed for new users: help
-            core.help(message, Discord);
-            break;
-        case "bytetest":
-            music.byteplTest(message, Discord, args, player);
-            break;
-        case "debug":
-            //Shows all the debug info for the sever
-            core.debug(message, Discord, client);
-            break;
-        default:
-            //Shows an error message
-            core.unknown(message, Discord, command);
-            break;
-        }
-    });
-
-// On voice state canged
-client.on('voiceStateUpdate', (oldMember, newMember) => {
-    // If parent bot is present, do nothing
-    if(parentId != "null") {
-        if(newMember.guild.members.fetch(parentId)) {
-            console.log("Handled by parent bot: " + parentId)
-            return;
-        }
+bot.on('ready', () => {
+  bot.user.setStatus('available')
+  bot.user.setPresence({
+    status: 'online',
+    activity: {
+        name: "you ;)",
+        type: "LISTENING",
     }
+  })
+});
 
-    let newUserChannel = newMember.channelID;
-    let oldUserChannel = oldMember.channelID;
-        
-    console.log(newUserChannel);
-    if(oldUserChannel == undefined && newUserChannel != undefined && newMember.member.user.bot != true) {
+bot.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channelId
+    let oldUserChannel = oldMember.channelId
     
+    console.log(newUserChannel)
+    if(oldUserChannel == undefined && newUserChannel != undefined && newMember.member.user.bot != true) {
+
         // User Joins a voice channel
         const channel = newMember.guild.channels.cache.find(channel => channel.name === "vcupdates");
-    
+
         let guild = newMember.guild;
-        let member = guild.member(newMember.member.user);
-        let nickname = member ? member.displayName : null;
-    
+        let member = guild.members.fetch(newMember.member.id);
+
+        let nickname = member ? member.nickname : null;
+
+        console.log(nickname);
+
+        if(typeof nickname === "undefined") {
+            nickname = newMember.member.user.username;
+        }
+
         const exampleEmbed = new Discord.MessageEmbed()
-            .setColor(color)
+            .setColor('#0067f4')
             .setTitle(nickname + ' just joined ' + newMember.channel.name)
             .setAuthor(nickname, newMember.member.user.displayAvatarURL())
-            .setTimestamp();
-    
-        channel.send(exampleEmbed);
-       
-    } else if(newUserChannel === undefined && oldUserChannel != undefined){
-       
-        // User leaves a voice channel
-       
-    }
+            .setTimestamp()
+
+        channel.send({ embeds: [exampleEmbed] })
+   
+     } else if(newUserChannel === undefined && oldUserChannel != undefined){
+   
+       // User leaves a voice channel
+   
+     }
+
+
 })
 
-//Log the bot in with the token provided
-client.login(token); 
+bot.on('message', message => {
+  if (message.content == '/notify') {
+    if(message.member.voice.channel) {
+      const channel = bot.channels.cache.find(channel => channel.name === "vcupdates")
+
+      let member = message.member
+      let nickname = member ? member.displayName : null;
+      var members = member.voice.channel.members;
+      var otherMembers = []
+      members.forEach(member => {
+        if(member.displayName != nickname && !member.user.bot) {
+          otherMembers.push(member.displayName)
+        }
+      });
+      var numbers = members.size - otherMembers.length - 1
+      if(numbers == 1) {
+        var other = "other"
+      }else{
+        var other = "others"
+      }
+      var membersOut;
+      if(otherMembers.length == 1) {
+        membersOut = "with " + otherMembers[0];
+      }else if(otherMembers == 0) {
+        membersOut = "";
+      }else {
+        var final = otherMembers[otherMembers.length - 1];
+        otherMembers.pop();
+        membersOut = "with " + otherMembers.join(", ") + " and " + final
+      }
+      channel.send("@everyone, " + nickname + " is in **" + member.voice.channel.name + "** " + membersOut)
+    }else {
+      
+    
+      message.reply("you must be in a voice channel to run this command")
+    }
+  }
+});
