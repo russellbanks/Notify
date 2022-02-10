828b77fb238ac799c32fc8c52e5d350dfcf23ea7
Jack Devey <jack@bandev.uk>
2022-02-10 12:28:41 +0000

    Set dotenv to ignore if missing

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -3,7 +3,10 @@ import io.github.cdimascio.dotenv.dotenv
 
 object Config {
 
-    private val dotenv: Dotenv = dotenv()
+    private val dotenv: Dotenv = dotenv {
+        ignoreIfMalformed = true
+        ignoreIfMissing = true
+    }
 
     val discordApiKey = dotenv["DISCORD_API_KEY"] ?: error("Token required.")
     val mongoDbUri = dotenv["MONGODB_URI"] ?: error("Mongodb uri required.")
