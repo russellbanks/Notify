abd3238523557fde1a34a34e19d3570837c97026
Russell Banks <Russell@bandev.uk>
2022-02-11 17:31:55 +0000

    Bump Shadow to 7.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.6.10"
-    id("com.github.johnrengelman.shadow") version "7.0.0"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     application
 }
 
