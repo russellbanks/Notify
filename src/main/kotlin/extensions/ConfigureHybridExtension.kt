78d10ea193f4b6339aeaf5c1ec9f3b0c4161062f
Russell Banks <Russell@bandev.uk>
2022-03-15 14:29:51 +0000

    Add parent configure command

--- src/main/kotlin/extensions/ConfigureHybridExtension.kt
+++ src/main/kotlin/extensions/ConfigureHybridExtension.kt
@@ -24,6 +24,8 @@ import EnvironmentVariables
 import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.checks.isNotBot
 import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.chat.ChatCommandRegistry
+import com.kotlindiscord.kord.extensions.commands.chat.ChatGroupCommand
 import com.kotlindiscord.kord.extensions.commands.converters.impl.channel
 import com.kotlindiscord.kord.extensions.components.ComponentContainer
 import com.kotlindiscord.kord.extensions.components.components
@@ -36,6 +38,7 @@ import dev.kord.common.Color
 import dev.kord.common.entity.ButtonStyle
 import dev.kord.common.entity.ChannelType
 import dev.kord.common.entity.Permission
+import dev.kord.common.entity.Snowflake
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.entity.Guild
 import dev.kord.rest.Image
@@ -43,44 +46,80 @@ import dev.kord.rest.builder.message.create.embed
 import dev.kord.rest.builder.message.modify.embed
 import dev.kord.x.emoji.Emojis
 import extensions.voicestateupdate.Action
+import io.github.qbosst.kordex.builders.embed
 import io.github.qbosst.kordex.commands.hybrid.ephemeralHybridCommand
 import io.github.qbosst.kordex.commands.hybrid.ephemeralSubCommand
+import org.koin.core.component.inject
 
 class ConfigureHybridExtension: Extension() {
     override val name = "configure"
 
+    private val messageCommandsRegistry: ChatCommandRegistry by inject()
+
     override suspend fun setup() {
         ephemeralHybridCommand {
-            name = "Configure"
-            description = "Configure a server's preferences"
+            name = "configure"
+            description = "Show the server's current preferences"
 
             check { isNotBot() }
-            check { hasPermission(Permission.ManageGuild) }
+
+            action {
+                respond {
+                    guild?.let { guild ->
+                        embed {
+                            color = Color(EnvironmentVariables.accentColor()[0], EnvironmentVariables.accentColor()[1], EnvironmentVariables.accentColor()[2])
+                            title = guild.asGuild().name
+                            field("Notifications Channel") {
+                                kord.getChannel(Snowflake(Datastore.GuildPrefsCollection.get(guild.asGuild()).channelId))!!.mention
+                            }
+                            actionList.forEach { action ->
+                                field(action.name.lowercase().replaceFirstChar { it.titlecase()}) {
+                                    if (getActionToggle(action, guild.asGuild())) Emojis.whiteCheckMark.unicode else Emojis.x.unicode
+                                }
+                            }
+                            field("Subcommands:") {
+                                var subCommandsNames = ""
+                                messageCommandsRegistry.commands.forEach { chatCommand ->
+                                    if (chatCommand is ChatGroupCommand && chatCommand.name == name) {
+                                        chatCommand.commands.forEach {
+                                            subCommandsNames += if (it != chatCommand.commands.last()) "`${it.name}`, " else "`${it.name}`"
+                                        }
+                                    }
+                                }
+                                subCommandsNames
+                            }
+                        }
+                    }
+                }
+            }
 
             ephemeralSubCommand {
                 name = "notifications"
                 description = "Configure which events should trigger a notification"
 
+                check { hasPermission(Permission.ManageGuild) }
+
                 action {
-                    val guild = member?.getGuild()!!
-                    respond {
-                        content = "I have sent you the configuration options for this server via DM."
-                    }
-                    member?.getDmChannel()?.createMessage {
-                        embed {
-                            author {
-                                name = "${guild.name} configuration"
-                                icon = guild.getIconUrl(Image.Format.PNG)
-                            }
-                            color = Color(EnvironmentVariables.accentColor()[0], EnvironmentVariables.accentColor()[1], EnvironmentVariables.accentColor()[2])
-                            actionList.forEach { action ->
-                                field("${action.name.lowercase().replaceFirstChar { it.titlecase()} } ${if (getActionToggle(action, guild)) Emojis.whiteCheckMark.unicode else Emojis.x.unicode}")
-                            }
+                    guild?.let { guild ->
+                        respond {
+                            content = "I have sent you the configuration options for this server via DM."
                         }
-                        components {
-                            member?.let {
+                        member?.getDmChannel()?.createMessage {
+                            embed {
+                                author {
+                                    name = "${guild.asGuild().name} configuration"
+                                    icon = guild.asGuild().getIconUrl(Image.Format.PNG)
+                                }
+                                color = Color(EnvironmentVariables.accentColor()[0], EnvironmentVariables.accentColor()[1], EnvironmentVariables.accentColor()[2])
                                 actionList.forEach { action ->
-                                    publicActionButton(action, guild)
+                                    field("${action.name.lowercase().replaceFirstChar { it.titlecase()} } ${if (getActionToggle(action, guild.asGuild())) Emojis.whiteCheckMark.unicode else Emojis.x.unicode}")
+                                }
+                            }
+                            components {
+                                member?.let {
+                                    actionList.forEach { action ->
+                                        publicActionButton(action, guild.asGuild())
+                                    }
                                 }
                             }
                         }
@@ -91,6 +130,8 @@ class ConfigureHybridExtension: Extension() {
             ephemeralSubCommand(::ChannelArgs) {
                 name = "channel"
                 description = "Set which channel notifications should be sent in"
+
+                check { hasPermission(Permission.ManageGuild) }
 
                 action {
                     respond {
